d45dc8ea19895d754fdaf86fc6440e3386451611
GitMaster7 <52113266+GitMaster7@users.noreply.github.com>
2020-06-28 22:10:13 -0500

    Commit New

---
+++ precache-manifest.3b69e6e6a3699db07dccdcb55b24106b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ce899270885a96ec5ab02ef74367c47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e356071b33c284293d51",
+    "url": "/static/css/main.9ce8007f.chunk.css"
+  },
+  {
+    "revision": "1307bc5a5bb1fb114252",
+    "url": "/static/js/2.c9b88508.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c9b88508.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e356071b33c284293d51",
+    "url": "/static/js/main.ea7176da.chunk.js"
+  },
+  {
+    "revision": "064f728010e43d89aefc",
+    "url": "/static/js/runtime-main.8f696bfe.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
